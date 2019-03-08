53f8584c7e00bbd868d44bb4005aa32945c55ea8
TBCS\y-okayama <y-okayama@tribeck.jp>
2019-03-08 15:15:52 +0900

    post-css有効

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,9 @@ module.exports = {
   devServer: {
     open: true, //ブラウザを自動で開く
     openPage: 'index.html', //自動で指定したページを開く
-    contentBase: path.join(__dirname, 'public'), // HTML等コンテンツのルートディレクトリ
+    contentBase: path.join(__dirname, 'public'), // HTML等、コンテンツのルートディレクトリ
     watchContentBase: true, //コンテンツの変更監視をする
-    port: 3000, // ポート番号
+    port: 8080, // ポート番号
   },
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: {
@@ -71,6 +71,19 @@ module.exports = {
           },
           {
             loader: 'sass-loader',
+          },
+          // PostCSS(Autoprefix)
+          {
+            loader: 'postcss-loader',
+            options: {
+              // PostCSS側でもソースマップを有効にする
+              sourceMap: true,
+              plugins: [
+                // Autoprefixerを有効化
+                // ベンダープレフィックスを自動付与する
+                require('autoprefixer')({ grid: true })
+              ]
+            },
           }
         ],
       }
