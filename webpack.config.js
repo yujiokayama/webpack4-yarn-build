aa7d3ead510aa8347419ad513ede873060cb56ec
yuji okayama <y.okayama.13@gmail.com>
2019-03-10 23:46:04 +0900

    今度こそ一旦完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 // output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
 const path = require('path');
 // 'production' か 'development' を指定
-const MODE = 'production';
+const MODE = 'development';
 // plugin
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -19,14 +19,22 @@ module.exports = {
     openPage: 'index.html', //自動で指定したページを開く
     contentBase: path.join(__dirname, 'public'), // HTML等、コンテンツのルートディレクトリ
     watchContentBase: true, // コンテンツの変更監視をする
-    port: 8080 // ポート番号
+    port: 3000 // ポート番号
   },
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: {
     // main.js
-    'public/js/main': './src/ts/main.ts',
-    // scss
-    'public/css/style': './src/scss/style.scss'
+    'js/main': './src/ts/main.ts',
+    // scss(style.cssを出力)
+    'css/style': './src/scss/style.scss'
+  },
+  output: {
+    // 出力するファイル名
+    filename: '[name].js',
+    // __dirnameは webpack.config.js があるディレクトリの絶対パス
+    path: path.resolve(__dirname, './public/'),
+    //ブラウザからバンドルにアクセスする際のパス
+    publicPath: '/'
   },
   // 最適化
   optimization: {
@@ -45,14 +53,6 @@ module.exports = {
       })
     ]
   },
-  output: {
-    // 出力するファイル名
-    filename: '[name].js',
-    // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.resolve(__dirname, ''),
-    //ブラウザからバンドルにアクセスする際のパス
-    publicPath: '/js/'
-  },
   module: {
     rules: [
       // typescript
@@ -65,41 +65,74 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
+          MiniCssExtractPlugin.loader, // javascriptとしてバンドルせず css として出力する
           {
             loader: 'css-loader',
             options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
               url: false,
-              // ソースマップの利用有無
               sourceMap: MODE === 'development'
             }
           },
-          // PostCSSの設定
           {
             loader: 'postcss-loader',
             options: {
+              sourceMap: MODE === 'development',
               plugins: [
                 AutoPrefixer({
-                  browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4'],
-                  grid: true
+                  grid: true,
+                  browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4']
                 })
               ]
             }
           },
+
           {
             loader: 'sass-loader',
             options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
-              url: false,
-              // ソースマップの利用有無
               sourceMap: MODE === 'development'
             }
           }
         ]
       }
+      // scss
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     {
+      //       loader: MiniCssExtractPlugin.loader
+      //     },
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         // オプションでCSS内のurl()メソッドの取り込みを禁止する
+      //         url: false,
+      //         // ソースマップの利用有無
+      //         sourceMap: MODE === 'development'
+      //       }
+      //     },
+      //     // PostCSSの設定
+      //     {
+      //       loader: 'postcss-loader',
+      //       options: {
+      //         plugins: [
+      //           AutoPrefixer({
+      //             browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4'],
+      //             grid: true
+      //           })
+      //         ]
+      //       }
+      //     },
+      //     {
+      //       loader: 'sass-loader',
+      //       options: {
+      //         // オプションでCSS内のurl()メソッドの取り込みを禁止する
+      //         url: false,
+      //         // ソースマップの利用有無
+      //         sourceMap: MODE === 'development'
+      //       }
+      //     }
+      //   ]
+      // }
     ]
   },
   // import 文で .ts ファイルを解決するため
@@ -109,7 +142,7 @@ module.exports = {
   // cssをjsと別に出力する
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'public/css/style.css'
+      filename: '[name].css'
     })
   ]
 };
