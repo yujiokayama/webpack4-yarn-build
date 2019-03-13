0df0f7415df4f801b5fc634fc81e9fdbb4a4bf2c
yuji okayama <y.okayama.13@gmail.com>
2019-03-13 21:46:29 +0900

    bootstrap-scssインストール

--- webpack.config.js
+++ webpack.config.js
@@ -94,45 +94,6 @@ module.exports = {
           }
         ]
       }
-      // scss
-      // {
-      //   test: /\.scss$/,
-      //   use: [
-      //     {
-      //       loader: MiniCssExtractPlugin.loader
-      //     },
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         // オプションでCSS内のurl()メソッドの取り込みを禁止する
-      //         url: false,
-      //         // ソースマップの利用有無
-      //         sourceMap: MODE === 'development'
-      //       }
-      //     },
-      //     // PostCSSの設定
-      //     {
-      //       loader: 'postcss-loader',
-      //       options: {
-      //         plugins: [
-      //           AutoPrefixer({
-      //             browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4'],
-      //             grid: true
-      //           })
-      //         ]
-      //       }
-      //     },
-      //     {
-      //       loader: 'sass-loader',
-      //       options: {
-      //         // オプションでCSS内のurl()メソッドの取り込みを禁止する
-      //         url: false,
-      //         // ソースマップの利用有無
-      //         sourceMap: MODE === 'development'
-      //       }
-      //     }
-      //   ]
-      // }
     ]
   },
   // import 文で .ts ファイルを解決するため
